2b5c037c241ac4efe6027ee22082b6593138c03c
ipeterov <ipeterov1@gmail.com>
2023-10-07 08:23:10 +0400

    Change python package name py_holdem_hand_evaluator -> holdem_hand_evaluator

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use pyo3::prelude::*;
-use holdem_hand_evaluator::{Hand};
+use ::holdem_hand_evaluator::{Hand};
 
 #[pyfunction]
 fn evaluate_hand(hand_str: String) -> u16 {
@@ -11,7 +11,7 @@ fn evaluate_hand(hand_str: String) -> u16 {
 /// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
 /// import the module.
 #[pymodule]
-fn py_holdem_hand_evaluator(_py: Python, m: &PyModule) -> PyResult<()> {
+fn holdem_hand_evaluator(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(evaluate_hand, m)?)?;
     Ok(())
 }
