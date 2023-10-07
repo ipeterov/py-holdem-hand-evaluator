f29f434648c3298c306e56b4055a5bd69e947af1
ipeterov <ipeterov1@gmail.com>
2023-10-07 07:58:31 +0400

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+use pyo3::prelude::*;
+use holdem_hand_evaluator::{Hand};
+
+#[pyfunction]
+fn evaluate_hand(hand_str: String) -> u16 {
+  let hand = hand_str.parse::<Hand>().unwrap();
+  return hand.evaluate();
+}
+
+/// A Python module implemented in Rust. The name of this function must match
+/// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
+/// import the module.
+#[pymodule]
+fn py_holdem_hand_evaluator(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(evaluate_hand, m)?)?;
+    Ok(())
+}
